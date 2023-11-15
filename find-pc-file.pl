88268c523eaa7ee512a857eb4606bdab70342598
Kaixiang Xie <kxie@roblox.com>
2023-11-15 14:48:15 -0800

    find-pc-file

---
+++ find-pc-file.pl
@@ -0,0 +1,9 @@
+#!/usr/bin/env perl
+
+$fn = shift @ARGV;
+@PATH = split ':', `pkg-config --variable pc_path pkg-config`;
+push @PATH, split ':', @ENV{PKG_CONFIG_PATH};
+for (@PATH) {
+    chomp;
+    print "$_\n" if -e "$_/$fn";
+}
