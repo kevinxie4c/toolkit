e8bca71284425b2b834799b9a2659ae8ab56db09
Kaixiang Xie <kaixiang.xie@mail.mcgill.ca>
2024-02-21 20:05:22 -0500

    fix a mistake in find-pc

--- find-pc.pl
+++ find-pc.pl
@@ -2,7 +2,7 @@
 
 $fn = shift @ARGV;
 @PATH = split ':', `pkg-config --variable pc_path pkg-config`;
-push @PATH, split ':', @ENV{PKG_CONFIG_PATH};
+push @PATH, split ':', $ENV{PKG_CONFIG_PATH};
 for (@PATH) {
     chomp;
     print "$_\n" if -e "$_/$fn";
